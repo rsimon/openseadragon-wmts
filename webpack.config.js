b678d54310d4e517baf35f8881c773db41171636
Rainer Simon <aboutgeo@gmail.com>
2021-03-06 17:46:30 +0100

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const fs = require('fs');
+
+const APP_DIR = fs.realpathSync(process.cwd());
+
+const resolveAppPath = relativePath => path.resolve(APP_DIR, relativePath);
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: resolveAppPath('src'),
+  output: {
+    filename: 'openseadragon-wmts.min.js',
+    library: ['OpenSeadragon', 'WMTS'],
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    pathinfo: true
+  },
+  externals: {
+    openseadragon: {
+      root: 'OpenSeadragon',
+      commonjs: 'openseadragon',
+      commonjs2: 'openseadragon',
+      amd: 'openseadragon'
+    }
+  },
+  performance: {
+    hints: false
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [ new UglifyJsPlugin({ sourceMap: true }) ],
+  },
+  resolve: {
+    extensions: ['.js' ]
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        use: { 
+          loader: 'babel-loader' ,
+          options: {
+            "presets": [
+              "@babel/preset-env"
+            ],
+            "plugins": [
+              [
+                "@babel/plugin-proposal-class-properties"
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },  
+  devServer: {
+    contentBase: resolveAppPath('public'),
+    compress: true,
+    hot: true,
+    host: process.env.HOST || 'localhost',
+    port: 3000,
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: resolveAppPath('public/index.html')
+    })
+  ]
+
+}
