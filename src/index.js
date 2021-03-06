1e9cdc12ac1099cebda403fc0df441e9bc66c8fb
Rainer Simon <aboutgeo@gmail.com>
2021-03-06 18:45:54 +0100

    Added WMTS capabilities parser from OpenLayers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import WMTSCapabilities from 'ol/format/WMTSCapabilities';
+
+const URL = 'https://wmts.maptiler.com/aHR0cDovL3dtdHMubWFwdGlsZXIuY29tL2FIUjBjSE02THk5dFlYQnpaWEpwWlhNdGRHbHNaWE5sZEhNdWN6TXVZVzFoZW05dVlYZHpMbU52YlM4eU5WOXBibU5vTDJ4aGJtTmhjMmhwY21VdmJXVjBZV1JoZEdFdWFuTnZiZy9qc29u/wmts';
+
+const parser = new WMTSCapabilities();
+
 const OpenSeadragonWMTS = () => {
-  
-  console.log('Hello World');
+    
+  console.log('Fetching WMTS capabilities');
+
+  fetch(URL)
+    .then(response => response.text())
+    .then(text => {
+      const result = parser.read(text);
+      console.log(result);
+    });
 
 }
 
