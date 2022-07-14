73eda725a77cc5733dd33e9ec2ef3e72804fb80b
Rainer Simon <aboutgeo@gmail.com>
2022-07-14 12:03:17 +0200

    Behavior tweak: map promise now returns after map has loaded (not after Capabilities have loaded)

--- src/index.js
+++ src/index.js
@@ -15,41 +15,47 @@ const OpenSeadragonWMTS = (viewer, args) => {
 
   const { url } = args;
 
-  return fetch(url)
-    .then(response => response.text())
-    .then(text => {
-      const { tileSource, mapBounds, projection } = parseCapabilities(text, args);
+  // Return after map has loaded
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => response.text())
+      .then(text => {
+        const { tileSource, mapBounds, projection } = parseCapabilities(text, args);
 
-      const topLeft = lonLatToViewportCoordinates(projection)([
-        Math.min(mapBounds[0], mapBounds[2]),
-        Math.max(mapBounds[3], mapBounds[1])
-      ]);
+        const topLeft = lonLatToViewportCoordinates(projection)([
+          Math.min(mapBounds[0], mapBounds[2]),
+          Math.max(mapBounds[3], mapBounds[1])
+        ]);
 
-      const bottomRight = lonLatToViewportCoordinates(projection)([
-        Math.max(mapBounds[2], mapBounds[0]),
-        Math.min(mapBounds[3], mapBounds[1])
-      ]);
+        const bottomRight = lonLatToViewportCoordinates(projection)([
+          Math.max(mapBounds[2], mapBounds[0]),
+          Math.min(mapBounds[3], mapBounds[1])
+        ]);
 
-      const viewportBounds = new Rect(topLeft[0], topLeft[1], bottomRight[0] - topLeft[0], bottomRight[1] - topLeft[1]);
+        const viewportBounds = new Rect(topLeft[0], topLeft[1], bottomRight[0] - topLeft[0], bottomRight[1] - topLeft[1]);
 
-      viewer.addTiledImage({ 
-        tileSource,        
-        success: () => {
-          viewer.viewport.fitBounds(viewportBounds, true);
-        }
+        viewer.addTiledImage({ 
+          tileSource,        
+          success: () => {
+            viewer.viewport.fitBounds(viewportBounds, true);
+
+            // Loaded - resolve promise
+            resolve({
+              imageToLonLat: imageToLonLat(viewer.viewport, projection),
+              lonLatToImageCoordinates: lonLatToImageCoordinates(viewer.viewport, projection),
+              lonLatToMapCoordinates: lonLatToMapCoordinates(projection),
+              lonLatToViewportCoordinates: lonLatToViewportCoordinates(projection),
+              mapToLonLat: mapToLonLat(projection),
+              mapToViewportCoordinates: mapToViewportCoordinates(projection.extent),
+              viewportToLonLat: viewportToLonLat(projection),
+              viewportToMapCoordinates: viewportToMapCoordinates(projection.extent)
+            });
+          },
+          error: e => reject(e)
+        });
+        
       });
-
-      return {
-        imageToLonLat: imageToLonLat(viewer.viewport, projection),
-        lonLatToImageCoordinates: lonLatToImageCoordinates(viewer.viewport, projection),
-        lonLatToMapCoordinates: lonLatToMapCoordinates(projection),
-        lonLatToViewportCoordinates: lonLatToViewportCoordinates(projection),
-        mapToLonLat: mapToLonLat(projection),
-        mapToViewportCoordinates: mapToViewportCoordinates(projection.extent),
-        viewportToLonLat: viewportToLonLat(projection),
-        viewportToMapCoordinates: viewportToMapCoordinates(projection.extent)
-      }
-    });
+  });
 
 }
 
